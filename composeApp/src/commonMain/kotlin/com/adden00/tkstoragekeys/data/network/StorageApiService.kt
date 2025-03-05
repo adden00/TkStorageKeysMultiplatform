894e8f38e67fbdbd1097b6d4b53eb73cc3a00a79
adden <a.d.denusiv@mail.ru>
2025-03-05 03:41:39 +0300

    removed cors try

--- composeApp/src/commonMain/kotlin/com/adden00/tkstoragekeys/data/network/StorageApiService.kt
+++ composeApp/src/commonMain/kotlin/com/adden00/tkstoragekeys/data/network/StorageApiService.kt
@@ -58,7 +58,6 @@ class StorageApiService(
             parameters.append("type", type)
         }
         val response = api.post(url.buildString()) {
-            headers.append("mode", "no-cors")
             contentType(ContentType.Application.Json)
             setBody(item)
         }
