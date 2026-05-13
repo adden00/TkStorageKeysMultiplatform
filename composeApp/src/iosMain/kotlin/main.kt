caef07ffbe036c928a1087dd829a32bbfaea8e2b
addenisov <a.d.denisov@mail.ru>
2026-05-13 14:45:31 +0300

    add ios support

--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -1,5 +1,9 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import com.adden00.tkstoragekeys.App
+import com.adden00.tkstoragekeys.di.initKoin
 import platform.UIKit.UIViewController
 
-fun MainViewController(): UIViewController = ComposeUIViewController { App() }
+fun MainViewController(): UIViewController {
+    initKoin()
+    return ComposeUIViewController { App() }
+}
