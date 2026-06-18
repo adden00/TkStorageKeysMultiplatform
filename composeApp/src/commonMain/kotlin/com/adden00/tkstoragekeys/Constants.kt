a37323d3c316001c4fe5a644359ae995e7f9f441
addenisov <a.d.denisov@mail.ru>
2026-06-19 02:33:00 +0300

    remove storage google api

--- composeApp/src/commonMain/kotlin/com/adden00/tkstoragekeys/Constants.kt
+++ composeApp/src/commonMain/kotlin/com/adden00/tkstoragekeys/Constants.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.unit.dp
 
 object Constants {
     val CORNERS_RADIUS = 15.dp
-    const val VERSION_CODE = 2
-    const val USE_SPRING_API = true
+    const val VERSION_CODE = 3
     const val SPRING_BASE_URL = "https://tkstorrage-api.fly.dev/"
 }
